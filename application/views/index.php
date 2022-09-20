2da01378b9d693671bc90a6e08f2422704a44fd8
bzndenis <bzndenis@gmail.com>
2022-09-20 11:19:50 +0700

    Update index.php

--- application/views/index.php
+++ application/views/index.php
@@ -6,8 +6,8 @@
     <div class="container" data-aos="zoom-out" data-aos-delay="100">
       <div class="row">
         <div class="col-xl-6">
-          <h1>Gelondong Emas Bogor</h1>
-          <h2>Gelondongan emas terpercaya sejak tahun xxx</h2>
+          <h1>Gelondong Emas Leonardi</h1>
+          <h2>Gelondongan Emas Terpercaya Sejak Tahun 2006</h2>
           <a href="#tabs" class="btn-get-started scrollto">Pelajari Lebih Lanjut</a>
         </div>
       </div>
@@ -63,39 +63,38 @@
 
         <div class="section-title">
           <h2>Fitur Pelayanan</h2>
-          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea.</p>
         </div>
 
         <div class="row">
           <div class="col-md-6">
             <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
               <i class="bi bi-briefcase"></i>
-              <h4><a href="#">Lorem Ipsum</a></h4>
-              <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+              <h4><a href="#">Integritas</a></h4>
+              <p>Integritas berarti kebenaran, menepati janji, dan memperlakukan orang lain dengan adil dan hormat. Integritas adalah salah satu aset kita yang paling berharga. Itu tidak boleh dikompromikan. </p>
             </div>
           </div>
           <div class="col-md-6 mt-4 mt-md-0">
             <div class="icon-box" data-aos="fade-up" data-aos-delay="200">
               <i class="bi bi-card-checklist"></i>
-              <h4><a href="#">Dolor Sitema</a></h4>
-              <p>Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
+              <h4><a href="#">Kualitas</a></h4>
+              <p>Kualitas ditunjukkan dalam banyak cara dengan menjual dan mendukung produk dan layanan yang menyenangkan pelanggan, membangun lingkungan kerja, dan memberikan hasil keuangan yang memenuhi harapan investor.</p>
             </div>
           </div>
           <div class="col-md-6 mt-4 mt-md-0">
             <div class="icon-box" data-aos="fade-up" data-aos-delay="300">
               <i class="bi bi-bar-chart"></i>
-              <h4><a href="#">Sed ut perspiciatis</a></h4>
-              <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+              <h4><a href="#">Komitmen</a></h4>
+              <p>Komitmen itu penting dimana kami akan terus memberikan yang terbaik kepada pelanggan dan investor kami sepanjang waktu. Kesempatan kita untuk melayani hendaknya dipandang sebagai hak istimewa yang tidak boleh diabaikan begitu saja.</p>
             </div>
           </div>
           <div class="col-md-6 mt-4 mt-md-0">
             <div class="icon-box" data-aos="fade-up" data-aos-delay="400">
               <i class="bi bi-binoculars"></i>
-              <h4><a href="#">Nemo Enim</a></h4>
-              <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+              <h4><a href="#">Inovasi</a></h4>
+              <p>Inovasi berarti menciptakan, merancang, dan mengembangkan produk dan layanan yang memiliki daya tarik tinggi di pasar dan memperkuat preferensi pelanggan untuk proyek Gelondong Emas Leonardi.</p>
             </div>
           </div>
-          <div class="col-md-6 mt-4 mt-md-0">
+          <!-- <div class="col-md-6 mt-4 mt-md-0">
             <div class="icon-box" data-aos="fade-up" data-aos-delay="500">
               <i class="bi bi-brightness-high"></i>
               <h4><a href="#">Magni Dolore</a></h4>
@@ -108,7 +107,7 @@
               <h4><a href="#">Eiusmod Tempor</a></h4>
               <p>Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
             </div>
-          </div>
+          </div> -->
         </div>
 
       </div>
