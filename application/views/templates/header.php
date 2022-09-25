57e310c766fd3222dd91470f7c52b9c8ddf5ad2d
bzndenis <bzndenis@gmail.com>
2022-09-25 12:38:14 +0700

    update icon

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -25,8 +25,10 @@
 	<meta property="twitter:image" content="assets/img/galery/p2.jpg">
 
   <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/favicon.ico" rel="icon">
   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+	<link href="assets/img/android-chrome-192x192.png" rel="android-chrome">
+	<link href="assets/img/android-chrome-512x512.png" rel="android-chrome">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
